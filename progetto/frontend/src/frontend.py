6723bcc655e391c5eaecde899a45a2fdfa4175e0
alex <ale06.angeletti@icloud.com>
2026-04-02 10:33:37 +0200

    Creazione json per i domini e le librerie richieste

---
+++ progetto/frontend/src/frontend.py
@@ -0,0 +1,17 @@
+import json
+from pathlib import Path
+
+
+
+
+BASE_DIR = Path(__file__).resolve().parent
+print(BASE_DIR)
+
+PROJECT_ROOT = BASE_DIR.parent.parent
+
+domains_path = PROJECT_ROOT / "domains.json"
+
+with open(domains_path, "r", encoding="utf-8") as f:
+    domains_data = json.load(f)
+
+print(domains_data)
